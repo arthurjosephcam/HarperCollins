00e4ddecb655df539147b91574627d307e932f5b
Arthur Cam <acam@financeofamerica.com>
2020-01-07 09:09:36 -0500

    Add project files.

---
+++ HarperCollins Service Interface/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarperCollins_Service_Interface
+{
+    public class Class1
+    {
+    }
+}
