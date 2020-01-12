644799f3c7e6b1944b9a9e3b282b953a89f5a5a6
Arthur Cam <acam@financeofamerica.com>
2020-01-12 03:26:49 -0500

    Added a symbolic unit test

---
+++ HarperCollins Service Tests/Mapper_Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HarperCollins.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace HarperCollins_Service_Tests
+{
+    [TestClass]
+    public class Mapper_Tests
+    {
+        protected IUnitOfWork UnitOfWork { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // Create Mock Repository Instance
+            UnitOfWork = Substitute.For<IUnitOfWork>();
+        }
+        [TestMethod]
+        public void SalesDataMapperTest()
+        {
+            //Here we can create a web application model sales data object, and compare with mapper results.
+        }
+    }
+}
---
+++ HarperCollins Service Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HarperCollins Service Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HarperCollins Service Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2aa3fb4e-df61-4600-adad-4b8cf2a498bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
